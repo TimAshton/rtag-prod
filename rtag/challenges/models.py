9fa5237863f508b73971b387b16aebb2f54b8d77
Tim <tim.ashton.la@gmail.com>
2026-03-11 13:49:25 -0700

    Add status to challenge model.

--- rtag/challenges/models.py
+++ rtag/challenges/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Challenge(models.Model):
-    challenge_title = models.CharField(max_length=60)
-    challenge_description = models.TextField()
-    challenge_created_date = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
